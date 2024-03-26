77ad13655ba8a7b2c487bbc32b31078f611c505a
cryptods8 <dusoo.s+ds8@gmail.com>
2024-03-26 08:51:41 +0100

    upgrade debugger

--- app/debug/hub/[...hubPath]/route.ts
+++ app/debug/hub/[...hubPath]/route.ts
@@ -1,12 +1,23 @@
-import { NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
+import { loadMockResponseForDebugHubRequest } from "../../utils/mock-hub-utils";
 
 function getHubRequest(request: NextRequest, hubPath: string[]) {
   const { url, headers: originalHeaders, ...rest } = request;
-
   const newUrl = new URL(url);
-  newUrl.protocol = "https";
-  newUrl.hostname = "api.neynar.com";
-  newUrl.port = "2281";
+
+  if (process.env.DEBUG_HUB_HTTP_URL) {
+    const hubUrl = new URL(process.env.DEBUG_HUB_HTTP_URL);
+    newUrl.protocol = hubUrl.protocol;
+    newUrl.hostname = hubUrl.hostname;
+    newUrl.port = hubUrl.port;
+  } else {
+    newUrl.protocol = "https";
+    newUrl.hostname = "hub-api.neynar.com";
+    newUrl.port = "443";
+  }
+
+  console.log("info: Mock hub: Forwarding message to", newUrl.toString());
+
   newUrl.pathname = hubPath.join("/");
 
   const headers = new Headers({
@@ -28,6 +39,15 @@ export async function GET(
   request: NextRequest,
   { params: { hubPath } }: { params: { hubPath: string[] } }
 ) {
+  const mockResponse = await loadMockResponseForDebugHubRequest(
+    request,
+    hubPath
+  );
+
+  if (mockResponse) {
+    return mockResponse;
+  }
+
   console.warn(
     `info: Mock hub: Forwarding message ${hubPath.join("/")} to a real hub`
   );
